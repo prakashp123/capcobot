220ba3480f485fd79a1f92d78d6cd10f0aaa1989
prakashp123 <prakash.pudhucode@capco.com>
2017-07-21 08:58:52 -0400

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -295,12 +295,14 @@ def getCustomerID(req):
     subject = parameters.get("subject")
     customerID = parameters.get("customerID")
     number = parameters.get("number")
-    if subject == "customer" and number < 100000 and number >-1:
+    '''if subject == "customer" and number < 100000 and number >-1:
         speech = "Okay. What KPI are you interested in seeing? " \
                  "If you do not know, type 'options for a list of options"
     else:
         speech = "This is not a valid customer ID. " \
-                 "Please reword, or type 'options' for a list of options."
+                 "Please reword, or type 'options' for a list of options."'''
+
+    speech = "Hi"
 
     return returnStatement(speech)
 
